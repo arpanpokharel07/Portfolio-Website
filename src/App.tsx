0fa8f8aa1ade5f03d3aca689718a3874ef980ef9
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2024-12-16 08:15:06 -0500

    Creating HomePage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Header from "./Component/Header"
+import Header from "./Component/Header";
+import HomePage from "./Component/HomePage";
 
 function App() {
-  
   return (
     <>
-    <Header></Header>
+      <Header></Header>
+      <HomePage></HomePage>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
