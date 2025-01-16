c8db25be4bfad9788dd33833f146e8102aa90d55
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2025-01-16 11:47:54 -0500

    Adding Education page

---
+++ src/Pages/Education.tsx
@@ -0,0 +1,35 @@
+const Education = () => {
+  return (
+    <div className="education">
+      <h1>Education</h1>
+
+      <div className="education-wrapper">
+        <div className="education-list">
+          <div className="bullet-point"></div>
+
+          <div className="year">May 2014 - April 2017</div>
+          <div className="institution-name">New Vision Academy</div>
+          <div className="address">Koteshower, Kathmandu, Nepal</div>
+          <div className="course">Junior School</div>
+          <br />
+          <div className="year">July 2017 - Oct 2019</div>
+          <div className="institution-name">Kathmandu Model College</div>
+          <div className="address">Bagbazar, Kathmandu, Nepal</div>
+          <div className="course">High School</div>
+          <br />
+          <div className="year">Jan 2021 - April 2022</div>
+          <div className="institution-name">Conestoga College</div>
+          <div className="address">Waterloo, Ontario, Canada</div>
+          <div className="course">Software Engineering Technician</div>
+          <br />
+          <div className="year">Sept 2022 - April 2024</div>
+          <div className="institution-name">Conestoga College</div>
+          <div className="address">Waterloo, Ontario, Canada</div>
+          <div className="course">Computer Programming</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
