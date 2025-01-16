73cf3aa552ad87a528c0fe1f6f38145c4912da48
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2025-01-16 11:47:24 -0500

    Completion of Contact Me page

--- src/Pages/ContactMe.tsx
+++ src/Pages/ContactMe.tsx
@@ -1,18 +1,25 @@
+import "./ContactMe.css";
 const ContactMe = () => {
   return (
     <div className="contact-me" id="contact-me">
       <h2>Any Project in mind? </h2>
-      <h4>Let's Get in Touch</h4>
-      <form>
+      <h4>Let's Get in Touch...</h4>
+      <form className="form">
         <label htmlFor="Name">Name: </label>
-        <input type="text" placeholder="Name" />
-
+        <input type="text" placeholder="Name (Example: James Bond)" />
+        <br />
         <label htmlFor="Email">Email: </label>
-        <input type="email" placeholder="Email" />
-
-        <label htmlFor="Message">Message: </label>
-        <textarea placeholder="Message"></textarea>
-
+        <input
+          type="email"
+          placeholder="Email (Example: jamesbond007@gmail.com)"
+        />
+        <br />
+        <label htmlFor=" Message">Message: </label>
+        <textarea
+          placeholder="Message (Example: Need a help for a confidential mission!!)"
+          rows={7}
+        ></textarea>
+        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
