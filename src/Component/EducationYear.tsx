ce4fe1d61e44f5bbfc95041a87f3d12c074f60f4
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2025-01-18 11:52:51 -0500

    Creating a year component to show year worked on

---
+++ src/Component/EducationYear.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type EducationProps = {
+  startYear: number;
+  endYear: number;
+};
+
+const EducationYear = (props: EducationProps) => {
+  return <div>EducationYear</div>;
+};
+
+export default EducationYear;
