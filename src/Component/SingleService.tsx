05718f711c74575105a9f8eb9f9662faf7985bff
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2025-01-30 22:05:25 -0500

    Redesigning of Service page

--- src/Component/SingleService.tsx
+++ src/Component/SingleService.tsx
@@ -1,4 +1,8 @@
+import * as Icons from "bootstrap-icons";
+import { Icon0Square, Icon0SquareFill } from "react-bootstrap-icons";
+
 type ServiceProps = {
+  logo: string;
   title: string;
   information: string;
 };
@@ -7,6 +11,9 @@ const SingleService = (props: ServiceProps) => {
   return (
     <div className="single-service">
       <div className="single-service-item">
+        <div className="logo">
+          <Icon0SquareFill size={50} />
+        </div>
         <div className="info">
           <h4>{props.title}</h4>
           <p>{props.information}</p>
--- src/Pages/Services.tsx
+++ src/Pages/Services.tsx
@@ -1,5 +1,4 @@
 import SingleService from "../Component/SingleService";
-import SingleServiceImage from "../Component/SingleServiceImage";
 import "./Services.css";
 import Header from "../Component/Header";
 
@@ -11,29 +10,31 @@ const Services = () => {
         <h1>Services</h1>
         <div className="services-wrapper">
           <div className="service-list">
-            <SingleServiceImage></SingleServiceImage>
             <SingleService
+              logo="images"
               title="Full-Stack Developer"
               information="I love building complete web applications, handling both the frontend and backend. On the frontend, I use React, and Tailwind CSS to create user-friendly, responsive interfaces. On the backend, I work with Node.js, Express.js, ASP.NET, and PHP, designing APIs and ensuring seamless data flow. I also manage databases like MySQL, MongoDB, and PostgreSQL and deploy applications using AWS, and Azure."
             ></SingleService>
             <SingleService
+              logo="images"
               title="Software Developer"
               information="Writing clean and efficient code is something I truly enjoy. I have experience with languages like C, C++, C#, Java, and Python  which allows me to develop software solutions across different platforms. Whether it’s backend logic, API development, or integrating systems, I focus on creating scalable and maintainable applications."
             ></SingleService>
-            <SingleServiceImage></SingleServiceImage>
-            <SingleServiceImage></SingleServiceImage>
+
             <SingleService
+              logo="images"
               title="Database Developer"
               information="A well-structured database is the foundation of any great application. I design, optimize, and manage databases using MySQL, MongoDB, and PostgreSQL, ensuring efficient data storage and retrieval. I also write optimized queries, improve performance through indexing, and handle data migration between different systems."
             ></SingleService>
             <SingleService
+              logo="images"
               title="Documentation Developer"
               information="Good documentation makes a huge difference in any project. I create clear, structured guides for APIs, databases, and software systems using tools like Markdown, Postman, and JIRA. My focus is on making technical concepts easy to understand, ensuring teams can work efficiently with well-documented processes."
             ></SingleService>
-            <SingleServiceImage></SingleServiceImage>
-            <SingleServiceImage></SingleServiceImage>
+
             <SingleService
-              title="Mobile Application Developer"
+              logo="images"
+              title="Mobile App Developer"
               information="Bringing applications to mobile is always exciting! I develop mobile apps using Android Studio (Java/Kotlin) and Swift, focusing on smooth user experiences and efficient performance. With my backend knowledge, I also build APIs and manage data interactions for mobile applications."
             ></SingleService>
           </div>
