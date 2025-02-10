9d0b2b06a5d985c57c5aed9039a9102a725ef9ce
Arpan Pokharel <83048924+arpanpokharel07@users.noreply.github.com>
2025-02-10 14:26:17 -0500

    Adding FocusFlow in Project Section

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -40,6 +40,21 @@ const secondProjectLanguages = [
   "/media/logos/nodejs.png",
 ];
 
+const thirdProjectImages = [
+  "/media/images/thirdProject-1.png",
+  "/media/images/thirdProject-2.png",
+  "/media/images/thirdProject-3.png",
+  "/media/images/thirdProject-4.png",
+  "/media/images/thirdProject-5.png",
+];
+const thirdProjectLanguages = [
+  "/media/logos/html5.png",
+  "/media/logos/css3.png",
+  "/media/logos/javascript.png",
+  "/media/logos/jquery.png",
+  "/media/logos/bootstrap.png",
+];
+
 const Projects = () => {
   const settings = {
     dots: true,
@@ -71,6 +86,14 @@ const Projects = () => {
                 subheading={secondProjectLanguages}
                 description="FilmyBox is a streaming platform designed to deliver a personalized, accessible, and engaging viewing experience worldwide. With advanced features and a well-planned marketing strategy, it aims to revolutionize the movie industry, capture a significant market share, and uphold high standards for both users and stakeholders."
                 info="https://www.linkedin.com/posts/jayminparmar2884_introducing-filmybox-our-capstone-project-activity-7185726800189128705-ehPf/?utm_source=share&utm_medium=member_desktop"
+              ></SingleProject>
+
+              <SingleProject
+                images={thirdProjectImages}
+                title="FocusFlow"
+                subheading={thirdProjectLanguages}
+                description="FocusFlow is a mobile-friendly web application designed to boost your productivity. It centralizes the management of long-term goals, daily habits, and to-do lists—providing you with a personalized companion to plan and track your progress. This project is created by Siddhart, Arpan and Rohit for Mobile Application to learn jQuery."
+                info="https://github.com/arpanpokharel07/FocusFlow"
               ></SingleProject>
             </Slider>
           </div>
